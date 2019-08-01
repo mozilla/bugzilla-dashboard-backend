9b821548de4f7e3c6721982af9809413e85ccf34
Bastien Abadie <bastien@nextcairn.com>
2019-08-01 12:07:08 +0200

    Enforce all setup.cfg rules

--- bugzilla_dashboard/__init__.py
+++ bugzilla_dashboard/__init__.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +11,6 @@ app = Flask(__name__)
 @app.errorhandler(404)
 def not_found(error):
     return {
-        "status": 404,
-        "message": "Not found"
+        'status': 404,
+        'message': 'Not found'
     }
